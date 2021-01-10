e6ba53da73b6c8245690609a7e0a59abf74c7ca2
pnbalu <pnbalu@yahoo.com>
2021-01-10 16:51:09 -0700

    New

---
+++ src/main/java/com/cyoa/service/EmailAlreadyUsedException.java
@@ -0,0 +1,11 @@
+package com.cyoa.service;
+
+public class EmailAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public EmailAlreadyUsedException() {
+        super("Email is already in use!");
+    }
+
+}
